694fe06dd5b819cb95aaae364620aef675c0585b
ImAadarsh <aadarshkavita@gmail.com>
2025-08-27 11:24:10 +0530

    AA

--- includes/header.php
+++ includes/header.php
@@ -9,7 +9,7 @@
     <meta name="description" content="SRN Seeds">
 
     <!-- ========== Page Title ========== -->
-    <title>SRN Seeds</title>
+    <title>SRN Seeds | SRNSEEDS.COM Jaipur, Rajasthan | FRAUDSTER COMPANY | ANSHUL SHARMA</title>
 
     <!-- ========== Favicon Icon ========== -->
     <link rel="shortcut icon" href="assets/img/favicon.png" type="image/x-icon">
